83fe24d2ac12e6b7ae0494e3929fc83fb8fb79e1
cwielder <160283747+cwielder@users.noreply.github.com>
2025-01-01 20:26:27 -0500

    Restructure pipeline
    also casually tripled performance with a pragma

--- LumiTracer/src/Renderer.cpp
+++ LumiTracer/src/Renderer.cpp
@@ -7,7 +7,7 @@
 #include <iostream>
 
 namespace {
-	static glm::u32 ConvertToRGBA(const glm::vec4& color) {
+	static glm::u32 convertToRGBA(const glm::vec4& color) {
 		glm::u8 r = static_cast<glm::u8>(color.r * 255.0f);
 		glm::u8 g = static_cast<glm::u8>(color.g * 255.0f);
 		glm::u8 b = static_cast<glm::u8>(color.b * 255.0f);
@@ -25,6 +25,9 @@ Renderer::Renderer()
 }
 
 void Renderer::Render(const Scene& scene, const Camera& camera) {
+	mActiveScene = &scene;
+	mActiveCamera = &camera;
+
 	const glm::u32vec2& viewport = camera.GetViewport();
 
 	if (viewport.x == 0 || viewport.y == 0) {
@@ -43,23 +46,47 @@ void Renderer::Render(const Scene& scene, const Camera& camera) {
 
 	std::cout << "Shading " << viewport.x * viewport.y << " pixels" << std::endl;
 
-	Ray ray{ };
-	ray.origin = camera.GetPosition();
-
-	for (std::uint_fast32_t y = 0; y < viewport.y; y++) {
-		for (std::uint_fast32_t x = 0; x < viewport.x; x++) {
-			ray.direction = camera.GetRayDirections()[x + y * viewport.x];
-
-			glm::vec4 color = this->TraceRay(scene, ray);
+	using i_t = std::int_fast32_t;
+	#pragma omp parallel for schedule(dynamic, 1) num_threads(8)
+	for (std::int_fast32_t y = 0; y < static_cast<i_t>(viewport.y); y++) {
+		for (std::int_fast32_t x = 0; x < static_cast<i_t>(viewport.x); x++) {
+			glm::vec4 color = this->PerPixel(x, y);
 			color = glm::clamp(color, { 0.0f }, { 1.0f });
-			mFinalImageData[x + y * viewport.x] = ConvertToRGBA(color);
+			mFinalImageData[x + y * viewport.x] = convertToRGBA(color);
 		}
 	}
 
 	mFinalImage->SetData(mFinalImageData);
+
+	mActiveScene = nullptr;
+	mActiveCamera = nullptr;
 }
 
-glm::vec4 Renderer::TraceRay(const Scene& scene, const Ray& ray) {
+glm::vec4 Renderer::PerPixel(const std::uint_fast32_t x, const std::uint_fast32_t y) {
+	Ray ray = {
+		.origin = mActiveCamera->GetPosition(),
+		.direction = mActiveCamera->GetRayDirections()[x + y * mActiveCamera->GetViewport().x]
+	};
+	
+	glm::vec3 accumulation = glm::vec3(0.0f);
+	for (int i = 0; i < 1; i++) {
+		const HitPayload payload = this->TraceRay(ray);
+
+		if (payload.hitDistance < 0.0f) {
+			break;
+		}
+
+		glm::vec3 lightDir = glm::normalize(glm::vec3(-1.0f, -1.0f, -1.0f));
+		glm::f32 lightIntensity = glm::max(glm::dot(payload.worldNormal, -lightDir), 0.0f);
+
+		const Material& material = mActiveScene->spheres[payload.objectIndex].material;
+		accumulation += material.albedo * lightIntensity;
+	}
+
+	return glm::vec4(accumulation, 1.0f);
+}
+
+Renderer::HitPayload Renderer::TraceRay(const Ray& ray) {
 	// (bx^2 + by^2)t^2 + (2(axbx + ayby))t + (ax^2 + ay^2 - r^2) = 0
 	// a = ray origin
 	// b = ray direction
@@ -68,13 +95,15 @@ glm::vec4 Renderer::TraceRay(const Scene& scene, const Ray& ray) {
 
 	constexpr glm::vec4 clearColor = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
 
-	if (scene.spheres.size() == 0) [[unlikely]] {
-		return clearColor;
+	if (mActiveScene->spheres.size() == 0) [[unlikely]] {
+		return this->Miss();
 	}
 
-	const Sphere* closestSphere = nullptr;
+	glm::u32 objectIndex = std::numeric_limits<glm::u32>::max();
 	glm::f32 hitDistance = std::numeric_limits<glm::f32>::max();
-	for (const auto& sphere : scene.spheres) {
+	for (int i = 0; i < mActiveScene->spheres.size(); i++) {
+		const auto& sphere = mActiveScene->spheres[i];
+
 		glm::vec3 origin = ray.origin - sphere.position;
 
 		glm::f32 a = glm::dot(ray.direction, ray.direction);
@@ -91,24 +120,37 @@ glm::vec4 Renderer::TraceRay(const Scene& scene, const Ray& ray) {
 
 		glm::f32 t1 = (-b - glm::sqrt(discriminant)) / (2.0f * a);
 
-		if (t1 < hitDistance) {
-			closestSphere = &sphere;
+		if (t1 > 0.0f && t1 < hitDistance) {
+			objectIndex = i;
 			hitDistance = t1;
 		}
 	}
 
-	if (closestSphere == nullptr) {
-		return clearColor;
+	if (objectIndex == std::numeric_limits<glm::u32>::max()) {
+		return this->Miss();
+	} else {
+		return this->ClosestHit(ray, hitDistance, objectIndex);
 	}
+}
 
-	glm::vec3 origin = ray.origin - closestSphere->position;
+Renderer::HitPayload Renderer::ClosestHit(const Ray& ray, const glm::f32 hitDistance, const glm::u32 objectIndex) {
+	const auto& sphere = mActiveScene->spheres[objectIndex];
 
-	glm::vec3 hitPoint = origin + ray.direction * hitDistance;
-	glm::vec3 normal = glm::normalize(hitPoint);
+	glm::vec3 origin = ray.origin - sphere.position;
 
-	glm::vec3 lightDir = glm::normalize(glm::vec3(-1.0f, -1.0f, -1.0f));
-	
-	glm::f32 d = glm::max(glm::dot(normal, -lightDir), 0.0f);
+	glm::vec3 worldPosition = origin + ray.direction * hitDistance;
+	glm::vec3 normal = glm::normalize(worldPosition);
 
-	return glm::vec4(closestSphere->material.albedo * d, 1.0f);
+	return {
+		.hitDistance = hitDistance,
+		.worldPosition = sphere.position + worldPosition,
+		.worldNormal = normal,
+		.objectIndex = objectIndex,
+	};
+}
+
+Renderer::HitPayload Renderer::Miss() {
+	return {
+		.hitDistance = -1.0f
+	};
 }
--- LumiTracer/src/Renderer.h
+++ LumiTracer/src/Renderer.h
@@ -19,8 +19,22 @@ public:
     [[nodiscard]] std::shared_ptr<Walnut::Image> GetFinalImage() { return mFinalImage; }
 
 private:
-    glm::vec4 TraceRay(const Scene& scene, const Ray& ray);
+    struct HitPayload {
+        glm::f32 hitDistance;
+        glm::vec3 worldPosition;
+        glm::vec3 worldNormal;
+        glm::u32 objectIndex;
+    };
 
+    glm::vec4 PerPixel(const std::uint_fast32_t x, const std::uint_fast32_t y);
+
+    HitPayload TraceRay(const Ray& ray);
+
+    HitPayload ClosestHit(const Ray& ray, const glm::f32 hitDistance, const glm::u32 objectIndex);
+    HitPayload Miss();
+private:
+    const Scene* mActiveScene = nullptr;
+    const Camera* mActiveCamera = nullptr;
     std::shared_ptr<Walnut::Image> mFinalImage;
     glm::u32* mFinalImageData;
 };
